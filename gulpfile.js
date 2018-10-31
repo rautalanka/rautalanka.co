c9e988e49ab59c11e6f5e7f6913af9027ee0a43b
rautalanka <rautalankaco@gmail.com>
2018-10-31 23:22:40 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,33 @@
 var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
-var sass = require("gulp-sass");
 
-// Static Server + watching scss/html files
-gulp.task("serve", ["sass"], function() {
-  browserSync.init({
-    server: "./app"
-  });
+gulp.task(
+  "css-browsersync",
+  gulp.series(function() {
+    return gulp
+      .src(["*.scss"])
+      .pipe(
+        sass().on("error", function(err) {
+          console.error(err.message);
+          browserSync.notify(err.message, 3000); // Display error in the browser
+          this.emit("end"); // Prevent gulp from catching the error and exiting the watch process
+        })
+      )
+      .pipe(gulp.dest("public/"))
+      .pipe(browserSync.stream());
+  })
+);
 
-  gulp.watch("app/scss/*.scss", ["sass"]);
-  gulp.watch("app/*.html").on("change", browserSync.reload);
-});
+gulp.task(
+  "watch",
+  gulp.series("css-browsersync", function() {
+    browserSync.init({
+      server: "./public"
+    });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function() {
-  return gulp
-    .src("app/scss/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
-});
+    gulp.watch("*.scss", ["css-browsersync"]);
+    gulp.watch("public/*.html").on("change", browserSync.reload);
 
-gulp.task("default", ["serve"]);
+    // protip: stop old version of gulp watch from running when you modify the gulpfile
+    gulp.watch("gulpfile.js").on("change", () => process.exit(0));
+  })
+);
